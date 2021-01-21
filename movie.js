bf260a6baf8c2c37d347e443021d1b1250ce2f87
codemail-sudhanshu <72255789+codemail-sudhanshu@users.noreply.github.com>
2021-01-21 12:41:52 +0530

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,120 @@
+const APIURL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+
+const sbar = document.getElementById("sb");
+
+const suggs = document.getElementById("suggs");
+
+const sbardiv = document.querySelector(".searchbar");
+
+function show() {
+  sbardiv.classList.add("sug");
+}
+function sugremove() {
+  sbardiv.classList.remove("sug");
+}
+
+sbar.addEventListener("click", function () {
+  SEARCH();
+});
+
+sbar.addEventListener("keyup", function (event) {
+  SEARCH(event);
+});
+
+sbar.addEventListener("focusout", function () {
+  console.log("blur");
+  sugremove();
+});
+
+const mc = document.getElementById("mc");
+
+const resp = new XMLHttpRequest();
+resp.open("GET", APIURL);
+resp.send();
+resp.onload = function () {
+  const movies = JSON.parse(resp.response).results;
+  showmovie(movies);
+};
+
+function showmovie(mov) {
+  const arr = Array.from(mov);
+  mc.innerHTML = "";
+
+  arr.forEach((move) => {
+    const mbox = document.createElement("div");
+
+    const { poster_path, title, vote_average } = move;
+    var ip = IMGPATH + poster_path;
+
+    mbox.classList.add("boxc");
+    console.log(poster_path);
+    if (poster_path === null) {
+      ip = "i.jpg";
+    }
+
+    mbox.innerHTML = ` <div class="box">
+                <div class="image">
+                    <img src="${ip}" class="img" alt="" />
+                </div>
+
+                <div class="title">
+                  ${title}
+                </div>
+            </div>
+            <div class="rating" style="background-color:${getcolor(
+              vote_average
+            )} ;">
+        ${vote_average}
+            </div>
+
+       `;
+
+    mc.appendChild(mbox);
+  });
+}
+
+function getcolor(num) {
+  if (num > 7) return "green";
+  else return "orange";
+}
+
+function showsuggestion(sg) {
+  suggs.innerHTML = "";
+  const sgar = Array.from(sg);
+  sgar.forEach((sg) => {
+    const sugdiv = document.createElement("a");
+    sugdiv.classList.add("sg");
+    sugdiv.href = "#";
+    sugdiv.innerHTML = `${sg.title}`;
+    suggs.appendChild(sugdiv);
+  });
+}
+
+function SEARCH(event) {
+  show();
+  const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${sbar.value}`;
+  const searching = new XMLHttpRequest();
+  searching.open("GET", SEARCHAPI);
+  searching.send();
+  searching.onload = function () {
+    const searchval = JSON.parse(searching.response).results;
+    var sugar = [];
+    sugar = Array.from(searchval);
+    showsuggestion(sugar);
+    if (sugar.length == 0) {
+      console.log(sugar.length);
+      const sudiv = document.createElement("div");
+      sudiv.classList.add("sg");
+      sudiv.href = "#";
+      sudiv.innerHTML = "No movies found";
+      suggs.appendChild(sudiv);
+    }
+
+    if (event.key === "Enter") {
+      showmovie(sugar);
+      sugremove();
+    }
+  };
+}
